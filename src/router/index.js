b6a8bb15a6eee4834e79241ce95ac489bbaf14d3
balejames <Elabjames@gmail.com>
2025-09-23 12:59:02 +0800

    feat: import ProductView component in router configuration

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/views/auth/LoginView.vue'
 import DashboardView from '@/views/DashboardView.vue'
+import ProductView from '@/views/ProductView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
