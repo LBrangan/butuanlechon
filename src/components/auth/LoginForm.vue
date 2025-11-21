098d3c532dfa5d9fd39a8512888e9d9c5b113b9a
Launcelot <lanceebrangan@gmail.com>
2025-11-21 23:06:28 +0800

    feat: added login/registerjs and updated loginform and loginview.

---
+++ src/components/auth/LoginForm.vue
@@ -0,0 +1,55 @@
+<script setup>
+  import { ref } from 'vue';
+  import { requiredValidator, emailValidator } from '@/utils/validators';
+  import { useLogin } from '../composables/auth/login';
+
+  const {formData, formAction, refVForm, onFormSubmit} = useLogin();
+
+  const isPasswordVisible =ref(false);
+</script>
+
+
+<template>
+  <div>
+      <v-form ref="refVForm" @submit.prevent="onFormSubmit">
+    <v-row dense>
+      <v-col cols="12">
+        <v-text-field
+          v-model="formData.email"
+          label="Email"
+          prepend-inner-icon="mdi-email-outline"
+          :rules="[requiredValidator, emailValidator]"
+        ></v-text-field>
+      </v-col>
+
+      <v-col cols="12">
+        <v-text-field
+          v-model="formData.password"
+          prepend-inner-icon="mdi-lock-outline"
+          label="Password"
+          :type="isPasswordVisible ? 'text' : 'password'"
+          :append-inner-icon="isPasswordVisible ? 'mdi-eye-off' : 'mdi-eye'"
+          @click:append-inner="isPasswordVisible = !isPasswordVisible"
+          :rules="[requiredValidator]"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+
+    <v-btn
+      class="mt-2"
+      type="submit"
+      color="red-darken-4"
+      prepend-icon="mdi-login"
+      :disabled="formAction.formProcess"
+      :loading="formAction.formProcess"
+      block
+    >
+      Login
+    </v-btn>
+  </v-form>
+  </div>
+</template>
+
+
+
+
